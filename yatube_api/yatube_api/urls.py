eb8f7361ee38a2d01e18776a0a0c1261db426e0e
Денис <denistischin@mail.ru>
2026-05-21 19:19:03 +0300

    api-yatube-main

--- yatube_api/yatube_api/urls.py
+++ yatube_api/yatube_api/urls.py
@@ -1,16 +1,16 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import include, path  # noqa
+from django.urls import include, path
 
 from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/', include('api.urls')),  # noqa
+    path('api/v1/', include('api.urls')),
     path('api/v1/api-token-auth/', obtain_auth_token),
-    path('auth/', include('djoser.urls')),  # noqa
-    path('auth/', include('djoser.urls.jwt')),  # noqa
+    path('auth/', include('djoser.urls')),
+    path('auth/', include('djoser.urls.jwt')),
 ]
 
 if settings.DEBUG:
